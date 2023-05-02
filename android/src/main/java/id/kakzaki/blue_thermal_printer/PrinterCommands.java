94a56c63d95608ced9158e57cc63f925ad1ac205
RetroAND <jlopezm@andorra.ad>
2023-05-02 10:17:03 +0200

    Added printer initialization; Fixed commands at definitions; Improved esc/pos command readability;

--- android/src/main/java/id/kakzaki/blue_thermal_printer/PrinterCommands.java
+++ android/src/main/java/id/kakzaki/blue_thermal_printer/PrinterCommands.java
@@ -5,7 +5,7 @@ package id.kakzaki.blue_thermal_printer;
  */
 
 public class PrinterCommands {
-    public static final byte HT = 0x9;
+    public static final byte HT = 0x09;
     public static final byte LF = 0x0A;
     public static final byte CR = 0x0D;
     public static final byte ESC = 0x1B;
@@ -14,56 +14,59 @@ public class PrinterCommands {
     public static final byte FS = 0x1C;
     public static final byte CAN = 0x18;
     public static final byte EOT = 0x04;
+    public static final byte NULL = 0x00;
 
-    public static final byte[] INIT = {27, 64};
-    public static byte[] FEED_LINE = {10};
+    public static final byte[] FEED_LINE = {LF};
 
-    public static byte[] SELECT_FONT_A = {20, 33, 0};
+    public static final byte[] SELECT_FONT_A = {ESC, 0x4D, 0x00};
 
-    public static byte[] SET_BAR_CODE_HEIGHT = {29, 104, 100};
-    public static byte[] PRINT_BAR_CODE_1 = {29, 107, 2};
-    public static byte[] SEND_NULL_BYTE = {0x00};
+    public static final byte[] SET_BAR_CODE_HEIGHT = {GS, 0x68};
+    public static final byte[] PRINT_UPCE_BAR_CODE = {GS, 0x6A, 0x02};
+    public static final byte[] SEND_NULL_BYTE = {NULL};
 
-    public static byte[] SELECT_PRINT_SHEET = {0x1B, 0x63, 0x30, 0x02};
-    public static byte[] FEED_PAPER_AND_CUT = {0x1D, 0x56, 66, 0x00};
+    public static final byte[] SELECT_PRINT_SHEET = {ESC, 0x63, 0x30, 0x02};
+    public static final byte[] FEED_PAPER_AND_CUT = {GS, 0x56, 0x42, 0x00};
 
-    public static byte[] SELECT_CYRILLIC_CHARACTER_CODE_TABLE = {0x1B, 0x74, 0x11};
+    public static final byte[] SELECT_CYRILLIC_CHARACTER_CODE_TABLE = {ESC, 0x74, 0x11};
 
-    public static byte[] SELECT_BIT_IMAGE_MODE = { 0x1B, 0x2A, 33, -128, 0 }; //{0x1B, 0x2A, 33, 0, 2};
-    public static byte[] SET_LINE_SPACING_24 = {0x1B, 0x33, 24};
-    public static byte[] SET_LINE_SPACING_30 = {0x1B, 0x33, 30};
+    public static final byte[] SELECT_BIT_IMAGE_MODE = {ESC, 0x2A, 0x21, 0xFF, 0x00 }; //{0x1B, 0x2A, 33, 0, 2};
+    public static final byte[] SET_LINE_SPACING = {ESC, 0x33};
 
-    public static byte[] TRANSMIT_DLE_PRINTER_STATUS = {0x10, 0x04, 0x01};
-    public static byte[] TRANSMIT_DLE_OFFLINE_PRINTER_STATUS = {0x10, 0x04, 0x02};
-    public static byte[] TRANSMIT_DLE_ERROR_STATUS = {0x10, 0x04, 0x03};
-    public static byte[] TRANSMIT_DLE_ROLL_PAPER_SENSOR_STATUS = {0x10, 0x04, 0x04};
+    public static final byte[] TRANSMIT_DLE_PRINTER_STATUS = {DLE, EOT, 0x01};
+    public static final byte[] TRANSMIT_DLE_OFFLINE_PRINTER_STATUS = {DLE, EOT, 0x02};
+    public static final byte[] TRANSMIT_DLE_ERROR_STATUS = {DLE, EOT, 0x03};
+    public static final byte[] TRANSMIT_DLE_ROLL_PAPER_SENSOR_STATUS = {DLE, EOT, 0x04};
 
-    public static final byte[] ESC_FONT_COLOR_DEFAULT = new byte[] { 0x1B, 'r',0x00 };
-    public static final byte[] FS_FONT_ALIGN = new byte[] { 0x1C, 0x21, 1, 0x1B,
-            0x21, 1 };
-    public static final byte[] ESC_ALIGN_LEFT = new byte[] { 0x1b, 'a', 0x00 };
-    public static final byte[] ESC_ALIGN_RIGHT = new byte[] { 0x1b, 'a', 0x02 };
-    public static final byte[] ESC_ALIGN_CENTER = new byte[] { 0x1b, 'a', 0x01 };
-    public static final byte[] ESC_CANCEL_BOLD = new byte[] { 0x1B, 0x45, 0 };
+    public static final byte[] ESC_FONT_COLOR_DEFAULT = new byte[] { ESC, 0x72,0x00 };
+    public static final byte[] FS_FONT_ALIGN = new byte[] { FS, 0x21, 0x01, 0x1B,
+            0x21, 0x01 };
+    public static final byte[] ESC_ALIGN_LEFT = new byte[] { ESC, 0x61, 0x00 };
+    public static final byte[] ESC_ALIGN_RIGHT = new byte[] { ESC, 0x61, 0x02 };
+    public static final byte[] ESC_ALIGN_CENTER = new byte[] { ESC, 0x61, 0x01 };
+    public static final byte[] ESC_CANCEL_BOLD = new byte[] { ESC, 0x45, 0x00 };
 
 
     /*********************************************/
-    public static final byte[] ESC_HORIZONTAL_CENTERS = new byte[] { 0x1B, 0x44, 20, 28, 00};
-    public static final byte[] ESC_CANCLE_HORIZONTAL_CENTERS = new byte[] { 0x1B, 0x44, 00 };
+    public static final byte[] ESC_HORIZONTAL_CENTERS = new byte[] { ESC, 0x44, 0x14, 0x1C, 0x00};
+    public static final byte[] ESC_CANCLE_HORIZONTAL_CENTERS = new byte[] { ESC, 0x44, 0x00 };
     /*********************************************/
 
     /*********** Open Cash Drawer ****************/    
-    public static final byte[] ESC_DRAWER_PIN2 = new byte[] { 0x1B, 'p', 0x30 };
-    public static final byte[] ESC_DRAWER_PIN5 = new byte[] { 0x1B, 'p', 0x31 };
+    public static final byte[] ESC_DRAWER_PIN2 = new byte[] { ESC, 0x70, 0x30 };
+    public static final byte[] ESC_DRAWER_PIN5 = new byte[] { ESC, 0x70, 0x31 };
     /*********************************************/
 
-    public static final byte[] ESC_ENTER = new byte[] { 0x1B, 0x4A, 0x40 };
-    public static final byte[] PRINTE_TEST = new byte[] { 0x1D, 0x28, 0x41 };
+    public static final byte[] ESC_ENTER = new byte[] { ESC, 0x4A, 0x40 };
+    public static final byte[] PRINTE_TEST = new byte[] { GS, 0x28, 0x41 };
 
     /**************** Encoding *******************/
-    static final byte [] ENC_ESP1 = new byte[] {0x1B, 0x52, 0x07}; //Bixolon esp1
-    static final byte [] ENC_ESP2 = new byte[] {0x1B, 0x52, 0x0B}; //Bixolon esp2
-    static final byte [] ENC_LAT1 = new byte[] {0x1B, 0x74, 0x10}; //Bixolon lat1
-    static final byte [] ENC_LAT9 = new byte[] {0x1B, 0x74, 0x30}; //Bixolon lat9
+    static final byte [] ENC_ESP1 = new byte[] {ESC, 0x52, 0x07}; //Bixolon esp1
+    static final byte [] ENC_ESP2 = new byte[] {ESC, 0x52, 0x0B}; //Bixolon esp2
+    static final byte [] ENC_LAT1 = new byte[] {ESC, 0x74, 0x10}; //Bixolon lat1
+    static final byte [] ENC_LAT9 = new byte[] {ESC, 0x74, 0x30}; //Bixolon lat9
+    /*********************************************/
+
+    /**************** Control *******************/
+    static final byte[] INIT_SEQ = new byte[] {ESC, 0x40};
     /*********************************************/
 }
